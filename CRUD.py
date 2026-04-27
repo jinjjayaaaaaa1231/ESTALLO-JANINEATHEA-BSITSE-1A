21330e6b3e2508eec97a77ed3ef5ec5caa785677
Janine Athea P. Estallo <247132030+jinjjayaaaaaa1231@users.noreply.github.com>
2026-04-28 07:53:22 +0800

    Implement basic CRUD operations in CRUD.py

---
+++ CRUD.py
@@ -0,0 +1,12 @@
+print("CRUD Practice")
+print("================")
+
+student_list= ['Andrei', 'Kassandra', 'Sharmine', 'Philip', 'Mary Jane']
+
+student_list.append("John Vinzent")
+student_list.insert(2,"Janine Athea")
+student_list.remove('Andrei')
+print(student_list.pop(-1))
+
+print(student_list)
+print(len(student_list))
